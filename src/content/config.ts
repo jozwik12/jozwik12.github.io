9afd7bc938266cd4ce21d6a46bc6110cb4c1a60c
jozwik12 <78445405+jozwik12@users.noreply.github.com>
2024-06-18 12:12:04 +0200

    cleanup

--- src/content/config.ts
+++ src/content/config.ts
@@ -5,7 +5,6 @@ const blog = defineCollection({
 	// Type-check frontmatter using a schema
 	schema: z.object({
 		title: z.string(),
-		description: z.string(),
 		// Transform string to Date object
 		pubDate: z.coerce.date(),
 		updatedDate: z.coerce.date().optional(),

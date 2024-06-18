415d8ccaf5fc09b07392d05c337faac0ad3cfb71
jozwik12 <78445405+jozwik12@users.noreply.github.com>
2024-06-18 12:33:50 +0200

    add page title

--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SAMPLE_CONST = 'foo';
+export const PAGE_TITLE = 'Szymon Józwicki';
